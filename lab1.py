c79ae4369b9a6d5d266d21e760998656d3f66ba2
Iovite Bogdan <iovite_bogdan@yahoo.com>
2021-06-26 19:51:48 +0300

    Update lab1.py
    finished lab1

--- lab1.py
+++ lab1.py
@@ -162,5 +162,17 @@ def problem_10():
         counter = len(inputstring.split())
         print(counter)
     countWords()
-problem_10()
+
+
+# problem_1()
+# problem_2()
+# problem_3()
+# problem_4()
+# problem_5()
+# problem_6()
+# problem_7()
+# problem_8()
+# problem_9()
+# problem_10()
+
         
